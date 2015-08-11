70a006740a601d1787b4055baa8db7891f7b62ad
Chris Steeb <chris.steeb.001@gmail.com>
2015-08-11 15:40:27 +0000

    Assignment 31 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,14 +15,24 @@ posts = Post.all
         body: Faker::Lorem.paragraph
     )
 end
+
+1.times do
+    Post.find_or_create_by(
+        title: "A Unique Title!",
+        body: "This is a very unique post."
+    )
+end
+posts = Post.all
+
+1.times do
+    Comment.find_or_create_by(
+        post: posts.sample,
+        body: "This is a unique comment."
+    )
+end
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
         
-        # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
