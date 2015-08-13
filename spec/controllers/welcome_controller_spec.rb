528a43152ed70df7a3a9f65011391a1e9affab14
Chris Steeb <chris.steeb.001@gmail.com>
2015-08-13 18:53:48 +0000

    Assignment TESTING completed

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -6,14 +6,25 @@ RSpec.describe WelcomeController, type: :controller do
             
             get :index
             
-            expect(response). to render_template("index")
+            expect(response).to render_template("index")
         end
     end
     
-    describe "GET about" do
+    describe "Get about" do
         it "renders the about template" do
+            
         get :about
+        
         expect(response).to render_template("about")
+        end
+    end
+    
+    describe "Get faq" do
+        it "renders the faq template" do
+            
+        get :faq
+            
+        expect(response).to render_template("faq")
         end
     end
 end
