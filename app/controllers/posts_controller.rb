a796efc60424c8325db1e1c757982fcac9e4ed24
Chris Steeb <chris.steeb.001@gmail.com>
2015-08-15 00:10:15 +0000

    Checkpoint CRUD complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,14 @@
 class PostsController < ApplicationController
-    def index
-     @posts = Post.all
-    end
+  def index
+    @posts = Post.all
+  end
 
-    def show
-    end
+  def show
+  end
 
-    def new
-    end
+  def new
+  end
 
-    def edit
-    end
+  def edit
+  end
 end
