0dcf7373f9f186888b61213275030372e26e761a
Chris Steeb <chris.steeb.001@gmail.com>
2015-08-15 00:57:41 +0000

    Assignment CRUD, in progress

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,16 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
   end
 
   def edit
+  end
+  
+  def censored
+    @posts = Post.select {|x| x % 5 == 0}
   end
 end
