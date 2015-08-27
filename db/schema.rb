0750f421fcdf0a7b50a8be18fbaf7407c206f937
Chris Steeb <chris.steeb.001@gmail.com>
2015-08-27 05:38:27 +0000

    Checkpoint 35 complete

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150823155645) do
 
-  create_table "advertisements", force: :cascade do |t|
-    t.string   "title"
-    t.text     "copy"
-    t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "post_id"
@@ -43,9 +35,9 @@ ActiveRecord::Schema.define(version: 20150823155645) do
   create_table "questions", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.boolean  "resolved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "resolved",   default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "topics", force: :cascade do |t|
