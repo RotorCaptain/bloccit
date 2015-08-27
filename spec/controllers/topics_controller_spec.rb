30d5b0b9d015c3900f54402cf090c2c92a831ce8
Chris Steeb <chris.steeb.001@gmail.com>
2015-08-27 03:56:23 +0000

    Assignment 34 In Progress

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -4,17 +4,38 @@ include RandomData
 RSpec.describe TopicsController, type: :controller do
   let (:my_topic) { Topic.create(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
     
-  describe "GET index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-        
-    it "returns http success" do
-      get :index
-      pect(assigns(:topics)).to eq([my_topic])
-    end
-  end
+  describe "GET new" do
+     it "returns http success" do
+       get :new
+       expect(response).to have_http_status(:success)
+     end
+ 
+     it "renders the #new view" do
+       get :new
+       expect(response).to render_template :new
+     end
+ 
+     it "initializes @topic" do
+       get :new
+       expect(assigns(:topic)).not_to be_nil
+     end
+   end
+   
+   describe "POST create" do
+     it "increases the number of topics by 1" do
+       expect{ post :create, {topic: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}}.to change(Topic,:count).by(1)
+     end
+   
+     it "assigns Topic.last to @topic" do
+       post :create, {topic: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}
+       expect(assigns(:topic)).to eq Topic.last
+     end
+ 
+     it "redirects to the new topic" do
+       post :create, {topic: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}
+       expect(response).to redirect_to Topic.last
+     end
+   end
     
   describe "GET show" do
     it "returns http success" do
@@ -50,22 +71,6 @@ RSpec.describe TopicsController, type: :controller do
     end
   end
     
-  describe "POST create" do
-      it "increases the number of topics by 1" do
-        expect{ post :create, {topic: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}}.to change(Topic,:count).by(1)
-      end
-   
-      it "assigns Topic.last to @topic" do
-        post :create, {topic: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}
-        expect(assigns(:topic)).to eq Topic.last
-      end
- 
-      it "redirects to the new topic" do
-        post :create, {topic: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}
-        expect(response).to redirect_to Topic.last
-      end
-    end
-  
   describe "GET edit" do
     it "returns http success" do
       get :edit, {id: my_topic.id}
