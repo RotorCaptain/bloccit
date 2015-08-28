ff465092ae9a72dff5075ec26c912a2f7c93ed33
Chris Steeb <chris.steeb.001@gmail.com>
2015-08-28 19:16:26 +0000

    Assignment 35 complete

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -3,26 +3,32 @@ require 'rails_helper'
 include RandomData
 
 RSpec.describe Topic, type: :model do
-   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
  
- it { should have_many(:posts) }
+  it { should have_many(:posts) }
 
-   context "attributes" do
-     it "should respond to name" do
-       expect(topic).to respond_to(:name)
-     end
+  context "attributes" do
+  it "should respond to name" do
+    expect(topic).to respond_to(:name)
+  end
  
-     it "should respond to description" do
-       expect(topic).to respond_to(:description)
-     end
+  it "should respond to description" do
+    expect(topic).to respond_to(:description)
+  end
  
-     it "should respond to public" do
-       expect(topic).to respond_to(:public)
-     end
+  it "should respond to public" do
+    expect(topic).to respond_to(:public)
+  end
  
 
-     it "should be public by default" do
-       expect(topic.public).to be(true)
-     end
-   end
- end
+  it "should be public by default" do
+    expect(topic.public).to be(true)
+  end
+     
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+    
+  it { should validate_length_of(:name).is_at_least(5) }
+  it { should validate_length_of(:description).is_at_least(20) }
+  end
+end
