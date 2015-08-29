5c7c38950445f3eff9e6c277bbfc6e01e47b0087
Chris Steeb <chris.steeb.001@gmail.com>
2015-08-29 12:27:56 +0000

    Assignment 37 completed

--- config/routes.rb
+++ config/routes.rb
@@ -13,11 +13,17 @@ Rails.application.routes.draw do
     
     resources :posts, except: [:index]
     
+    resources :sponsored_posts, except: [:index]
    end
 
   resources :users, only: [:new, :create]
   
-   get 'about' => 'welcome#about'
+  resources :advertisements
+  resources :questions
+  
+  post 'users/confirm' => 'users#confirm'
+  
+  get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
  
