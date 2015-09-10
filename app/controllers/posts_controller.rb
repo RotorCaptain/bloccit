596cc827dc5dcf03da789fb25079c68342895d92
Chris Steeb <chris.steeb.001@gmail.com>
2015-09-10 16:57:43 +0000

    CP 42 In Progress

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,102 +1,61 @@
 class PostsController < ApplicationController
 
-  
-  before_action :require_sign_in, expect: :show
-  before_action :authorize_user, except: [:show, :new, :create]
-  
-  def index
-    @posts = Post.all
-  end
-  
-
-  before_action :require_sign_in, except: :show
-
-
   def show
-    @post = Post.find(params[:id])
+    set_post
+    set_topic
   end
 
   def new
-    @topic = Topic.find(params[:topic_id])
+    set_topic
     @post = Post.new
+   
   end
 
   def create
-     @topic = Topic.find(params[:topic_id])
-     @post = @topic.posts.build(post_params)
-     @post.user = current_user
+    set_topic
+    @post = current_user.posts.build(post_params)
+    @post.topic = @topic
+    
 
-  
-     if @post.save
-  
-        flash[:notice] = "Post was saved."
-        redirect_to [@topic, @post]
-     else
-  
-        flash[:error] = "There was an error saving the post. Please try again."
-        render :new
-     end
-  end
-
-  def edit
-    @post = Post.find(params[:id])
-  end
-
-  def destroy
-    @post = Post.find(params[:id])
-     
-    if @post.destroy
-      flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+    if @post.save
+      flash[:notice] = "Post was saved."
         
-      redirect_to @post.topic
+      # redirect extracts the id from @post
+      redirect_to [@topic, @post]
     else
-      flash[:error] = "There was an error deleting the post."
-      render :show
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
     end
   end
   
-  private
-  
-  def post_params
-    params.require(:post).permit(:title, :body)
-  end
-  
-  def authorize_user
-     post = Post.find(params[:id])
-     unless current_user == post.user || current_user.admin?
-       flash[:error] = "You must be an admin to do that."
-       redirect_to [post.topic, post]
-     end
+  def edit
+    set_topic
+    set_post
   end
 
   def update
-     @post = Post.find(params[:id])
-     @post.assign_attributes(post_params)
-
-     if @post.save
-       flash[:notice] = "Post was updated."
-       redirect_to [@post.topic, @post]
-     else
-       flash[:error] = "There was an error saving the post. Please try again."
-       render :edit
-     end
+    set_topic
+    set_post
+    
+    if @post.update_attributes(post_params)
+        flash[:notice] = "Post was updated."
+        redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :edit
+    end
   end
 
-   def destroy
-     @post = Post.find(params[:id])
+  private
 
-     
-     if @post.destroy
-       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-       redirect_to @post.topic
-     else
-       flash[:error] = "There was an error deleting the post."
-       render :show
-     end
-   end
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 
-   private
-  def post_params
-     params.require(:post).permit(:title, :body)
-  end
+    
+
+    def set_topic
+      @topic = Topic.find(params[:topic_id])
+    end
+
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 include RandomData
 
 RSpec.describe Topic, type: :model do
-   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
- 
- it { should have_many(:posts) }
-
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+  
+  it { should have_many(:posts) }
+  it { should have_many(:labelings) }
+  it { should have_many(:labels).through(:labelings) }
+  
    context "attributes" do
      it "should respond to name" do
        expect(topic).to respond_to(:name)
